5dbcd850bee88fbe0819a327c7cbc6220459704a
4db444 <brahim77990alhyan@gmail.com>
2026-01-20 10:18:34 +0100

    removing unessery use statments from bootstrap file

--- App/Bootstrap/bootstrap.php
+++ App/Bootstrap/bootstrap.php
@@ -3,9 +3,6 @@
     require_once dirname(__DIR__) . "/Routes/web.php";
 
     use App\Core\Router;
-    use App\Core\Database;
-    use App\Core\Model;
-    use App\Core\QueryBuilder;
 
     $method = $_SERVER["REQUEST_METHOD"];
 
