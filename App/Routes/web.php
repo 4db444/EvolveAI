2b44805bf85f0326333c58f94a7c57ec22eff7ee
4db444 <brahim77990alhyan@gmail.com>
2026-01-26 16:30:05 +0100

    add new routes

--- App/Routes/web.php
+++ App/Routes/web.php
@@ -1,7 +1,7 @@
 <?php
     use App\Core\Router;
     use App\Controllers\AuthController;
-    use App\Controllers\OportunityControler;
+    use App\Controllers\OpportunityController;
     use App\Controllers\ProfilControler;
     use App\Controllers\DashboardControler;
     use App\Controllers\TasksControler;
@@ -12,7 +12,13 @@
 
     Router::Post("/auth/signup", [AuthController::class, "register"]);
     Router::Post("/auth/login", [AuthController::class, "authenticate"]);
+
     Router::Get("/dashboard", [DashboardControler::class, "dashboard"]);
     Router::Get("/profil", [ProfilControler::class, "profil"]);
     Router::Get("/tasks", [TasksControler::class, "tasks"]);
-    Router::Get("/oportunity", [OportunityControler::class, "oportunity"]);
+    Router::Get("/opportunity", [OpportunityController::class, "opportunity"]);
+
+    Router::get("/opportunity/{id}", [OpportunityController::class, "show"]);
+
+    Router::Post("/suggestions", [OpportunityController::class, "suggestions"]);
+    Router::Post("/opportunity/commit", [OpportunityController::class, "commit"]);
